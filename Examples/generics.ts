848eb8f80f906432b9d70acc9ca404ab039bc567
B <konta1231@gmail.com>
2025-05-26 18:52:16 +0530

    generics2

--- Examples/generics.ts
+++ Examples/generics.ts
@@ -122,3 +122,102 @@ function makeItMove<T extends Animal>(creature: T) {
 makeItMove(new Dog1());     // fine
 //makeItMove({});            //Error: Not an Animal
 //This ensures that only subclasses of Animal can be used.
+
+
+//Basic Mapped type
+
+console.log("4)   Basic mapped type generics")
+
+type Person1{
+  name: string;
+  age: number;
+}
+
+type Clone = {
+  [k in keyof Person1]: Person1[k];
+}
+// for each property key k in the type Person1, 
+// In simpler terms, it creates a type that mirrors the structure of Person1,
+//  mapping each property key to its corresponding value type.
+//keyof Person is 'name' | 'age'
+
+//Making All Properties Optional
+
+type Optional1<T> ={
+  [k in keyof T]?: T[k];
+}
+
+type Person2 ={
+  name:  string;
+  age: number;
+}
+
+type op= Optional1<Person2>;
+
+//Making All Properties Readonly
+
+type Readonly1<T> ={
+  readonly [k in keyof T]: T[k];
+}
+
+type Person3 ={
+  name:  string;
+  age: number;
+}
+
+type ReadonlyPerson= Readonly1<Person3>;
+
+
+// Use Case: Mapping with Transformed Types
+
+type Booleanified<T> = {
+  [K in keyof T]: boolean;
+};
+
+type Config = {
+  darkMode: string;
+  sidebarVisible: number;
+};
+
+type ConfigFlags = Booleanified<Config>;
+
+
+//Example with Generics and Utilities
+
+
+type Partiall<T> = {
+  [K in keyof T]?: T[K];
+};
+
+type Requiredl<T> = {
+  [K in keyof T]-?: T[K];
+};
+
+// -? removes optionality
+
+type Readonlyl<T> = {
+  readonly [K in keyof T]: T[K];
+};
+
+
+type Nullable<T> = {
+  [K in keyof T]: T[K] | null;
+};
+
+type Person4 = {
+  name: string;
+  age: number;
+};
+
+type NullablePerson = Nullable<Person4>;
+
+
+
+
+//Conditional types (T extends U ? X : Y)
+
+//Utility types (Partial, Pick, Omit, Record)
+
+//unknown and never types
+
+
