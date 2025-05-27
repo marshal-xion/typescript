4a900629b7c6e324bc6f6294d271b89a82f7d96b
Bishnu Prasad Chowdhury <bishnucit@gmail.com>
2025-05-27 16:32:39 +0530

    practice2

---
+++ Examples/27may25.ts
@@ -0,0 +1,51 @@
+
+function sumArray(numbers: number[]): number {
+    return numbers.reduce((sum, num) => sum + num, 0);
+}
+console.log(sumArray([1, 2, 3, 4])); // Output: 10
+console.log(sumArray([])); // Output: 0
+console.log(sumArray([5])); // Output: 5
+
+
+
+interface User {
+    name: string;
+    age: number;
+    isActive: boolean;
+}
+
+function getActiveUsers(users: User[]): User[]{
+    return users.filter(user => user.isActive);
+}
+
+
+const users: User[] = [
+    { name: "Alice", age: 25, isActive: true },
+    { name: "Bob", age: 30, isActive: false },
+    { name: "Charlie", age: 35, isActive: true }
+];
+
+console.log(getActiveUsers(users));
+let x = getActiveUsers(users)
+console.log(x[0]);
+console.log(x[0].name);
+console.log(x[0].age);
+console.log(x[1]);
+console.log(x[1].name);
+console.log(x[1].age);
+
+
+function reverseArray<T>(items: T[]): T[] {
+    return items.slice().reverse();
+}
+//  Why Use slice() Before reverse()?
+//Immutability: reverse() directly modifies the original array by rearranging its elements in reverse order.
+// If sparkle you want to keep the original array unchanged (which is often a good practice for predictable
+// code and avoiding side effects), you need to create a copy first.How slice() Helps: The slice() method
+// (with no arguments) returns a shallow copy of the entire array. This copy is a new array that you can
+// safely modify without affecting the original.
+
+
+console.log(reverseArray<number>([1, 2, 3, 4])); // Output: [4, 3, 2, 1]
+console.log(reverseArray<string>(["a", "b", "c"])); // Output: ["c", "b", "a"]
+console.log(reverseArray<boolean>([true, false, true])); // Output: [true, false, true]
