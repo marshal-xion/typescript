97099cf2be34c17b943c08e5d7e4314bc710acfc
Bishnu Prasad Chowdhury <bishnucit@gmail.com>
2025-06-04 08:59:13 +0530

    Practicecontinues

---
+++ Examples/4jun25.js
@@ -0,0 +1,19 @@
+//tsc .\4jun25.ts --target ES2015 --lib ES2015,DOM
+//Iterators and Generators
+class NumberIterator {
+    constructor(start, max) {
+        this.current = start;
+        this.max = max;
+    }
+    next() {
+        if (this.current <= this.max) {
+            return { value: this.current++, done: false };
+        }
+        return { value: undefined, done: true };
+    }
+}
+const iter = new NumberIterator(1, 3);
+console.log(iter.next()); // { value: 1, done: false }
+console.log(iter.next()); // { value: 2, done: false }
+console.log(iter.next()); // { value: 3, done: false }
+console.log(iter.next()); // { value: undefined, done: true }
